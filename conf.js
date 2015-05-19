2c4a9dee1000b67c72b3e43d0da31c6c1d069292
Julian Veling <julian_veling@hotmail.com>
2015-05-19 21:04:46 +0100

    travis with firefox

--- conf.js
+++ conf.js
@@ -2,4 +2,7 @@ exports.config = {
   seleniumAddress: "http://127.0.0.1:4444/wd/hub ",
   baseUrl: 'http://localhost:3000',
   specs: ['spec/features/cartFeaturesSpec.js'],
+  capabilities: {
+    'browserName': 'firefox'
+  }
 };
