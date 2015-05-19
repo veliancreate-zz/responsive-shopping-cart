1cd8bcd097cd4f69724f91917d7285b930d1511b
Julian Veling <julian_veling@hotmail.com>
2015-05-19 23:14:08 +0100

    tests refactor

--- spec/features/cartFeaturesSpec.js
+++ spec/features/cartFeaturesSpec.js
@@ -1,17 +1,27 @@
 'use strict';
 
 describe('Cart', function(){
+  
   describe('User can add order to shopping cart', function(){
+  
+    beforeEach(function(){
+      browser.get('/')
+    });
+
     it('should display all the products', function(){
-      browser.get('/');
       expect(element.all(by.repeater('product in products')).count()).toEqual(13);
     });
-    it('should display the right pages when you click links', function(){
-      browser.get('/');
-      var links = element.all(by.css('nav a'))
-      expect(element(by.css('h1')).getText()).toEqual('Threds Catalogue')
-      links.get(1).click();
-      expect(element(by.css('h1')).getText()).toEqual('My Cart')
+    
+    it('the home page h1 should be Threds Catalogue', function(){
+      expect(element(by.css('h1')).getText()).toEqual('Threds Catalogue');
     });
+
+    it('the cart page h1 should be My Cart', function(){
+      var links = element.all(by.css('nav a'));
+      links.get(1).click();
+      expect(element(by.css('h1')).getText()).toEqual('My Cart')      
+    });
+
   });
+
 });  
